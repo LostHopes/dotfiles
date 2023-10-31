c85c7c9a324657ae6901be477d4e5fc2f93c561e
losthope <arsen.melnuchyk@gmail.com>
2023-10-31 19:14:02 +0200

    added first version of install script

--- install.sh
+++ install.sh
@@ -1,3 +1,28 @@
 #!/bin/sh
 
+mv_dots(){
+
+    CONFIG_PATH=$(pwd)/.config
+    LOCAL_PATH=$(pwd)/.local
+
+
+    echo moving dots...
+    for item in $(\ls $CONFIG_PATH)
+    do
+        if [[ -d $HOME/.config/$item || -f $HOME/.config/$item ]]; then
+            echo $item exists
+        else
+            echo moving $item to $HOME/.config
+            cp -rt "$HOME/.config" "$(pwd)/.config/$item"
+        fi
+    done
+}
+
+
+read -p "Do you really want to install dotfiles? (y/n):" answer
+case $answer in
+    [Yy]* ) mv_dots; break;;
+    [Nn]* ) echo "Exiting..."; break;;
+    * ) echo "Please answer yes or no.";;
+esac
 
