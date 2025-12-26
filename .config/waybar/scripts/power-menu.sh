e8e1dddef7194009ac6e8b979d476148ec47c082
losthope <arsen.melnuchyk@gmail.com>
2025-12-27 01:12:20 +0200

    chore: init hyprland config

---
+++ .config/waybar/scripts/power-menu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+SCREEN_WIDTH=$(hyprctl monitors -j | jq '.[0].width')
+
+options="Shutdown\nReboot\nSleep"
+chosen=$(echo -e "$options" | wofi \
+    -dmenu \
+    -theme ~/.config/wofi/launcher.rasi \
+    -location 3 \
+    -anchor 3 \
+    -lines 3)
+
+case $chosen in
+    "Shutdown")
+        systemctl poweroff
+        ;;
+    "Reboot")
+        systemctl reboot
+        ;;
+    "Sleep")
+        systemctl suspend
+        ;;
+esac
